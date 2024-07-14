fb061465547a9dc460b8b73ebffc1c3158b3ea78
lojika-loji <kopalapillailojika@gmail.com>
2024-07-14 20:09:31 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.carousel').carousel({
+      interval: 1000,                 // Auto-play functionality with 1 seconds interval
+      pause: 'hover'                   // Pause on hover for desktop
+    });
+  });
+  
